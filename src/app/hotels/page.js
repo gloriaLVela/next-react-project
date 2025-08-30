e4546143b2dafa607f4ce87bc23151f546b5a1bb
gloriaLVela <gloria.laura.vela@gmail.com>
2025-08-30 16:47:18 -0500

    Passing data as props

--- src/app/hotels/page.js
+++ src/app/hotels/page.js
@@ -1,9 +1,29 @@
-export default function Page() {
+async function getData() {
+    const res = await fetch(
+        "https://snowtooth-api-rest.fly.dev/hotels"
+    );
+    return res.json();
+}
+
+function HotelBlock({ name, capacity }) {
+    return (
+        <div >
+            <h2>{name}</h2>
+            <p>{capacity}</p>
+        </div>
+    );
+}
+
+export default async function Page() {
+    const data = await getData();
     return (
         <main>
             <div>
                 <h1>Hotels Details</h1>
             </div>
+            <div>{data.map((hotel) => (
+                <HotelBlock key={hotel.id} name={hotel.name} capacity={hotel.capacity} />
+            ))}</div>
         </main>
     )
 }
